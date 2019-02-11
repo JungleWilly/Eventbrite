fac012af0c1272c7574e01a0f0ad0f0778ef077f
JungleWilly <guillaumec.dev@gmail.com>
2019-02-11 17:47:06 +0100

    migration et model OKAY, next test

---
+++ app/models/event.rb
@@ -0,0 +1,30 @@
+class Event < ApplicationRecord
+  has_many :attendances
+  has_many :users, through: :attendances
+ 
+  validates :start_date, presence: true
+
+  validates :duration, presence: true, numericality: {greater_than: 0}
+
+  validates :title, presence: true, length: {minimum:5, maximum:140}
+
+  validates :description, presence: true, length: {minimum:20, maximum:1000}
+
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000 }
+
+  validates :location, presence: true
+  validate :multiple_of_5
+  validate :expiration_date_cannot_be_in_the_past
+ 
+  
+  def expiration_date_cannot_be_in_the_past
+    unless start_date.present? && start_date > Date.today
+    end
+  end
+
+  def multiple_of_5
+    unless duration.present? && duration % 5 == 0
+    end
+  end
+  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
+require 'faker'
+
+10.times do
+  User.create!(email: Faker::Internet.email, encrypted_password: Faker::BreakingBad.episode, description: Faker::ChuckNorris.fact, first_name: Faker::FunnyName.name, last_name: Faker::DrWho.catch_phrase)
+end
+
+15.times do
+  Event.create!(start_date: Faker::Time.between(DateTime.now, DateTime.now + 1), duration: 5, title: Faker::Movie.quote, description: Faker::Lorem.characters(25), price: Faker::Number.between(1, 1000), location: Faker::Address.city)
+end
+
+30.times do
+  Attendance.create!(stripe_customer_id: Faker::Lorem.sentence, event: Event.all.sample, user: User.all.sample)
+end
