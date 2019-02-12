69d1d5e348e32b916a0a21c186685b9af442cc84
JungleWilly <guillaumec.dev@gmail.com>
2019-02-12 20:15:50 +0100

    navbar okay avec view devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   root 'events#index'
-  get 'events/index'
-  get 'events/new'
-  get 'events/create'
-  get 'events/show'
-  get 'events/edit'
-  get 'events/update'
-  get 'events/destroy'
-  root 'events#index'
+
   devise_for :users
   resources :events
+  resources :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
